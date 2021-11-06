1c38bcf7d20e6c8e560cf04030fd5acea1f62a7e
F7Villani <felipevillani.2000@hotmail.com>
2021-11-06 16:39:09 -0300

    Update RouterMovie.kt

--- src/main/java/controllers/ktor/routers/RouterMovie.kt
+++ src/main/java/controllers/ktor/routers/RouterMovie.kt
@@ -1,10 +1,13 @@
 package controllers.ktor.routers
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import fabrics.controllers.ktor.KtorControllerFabric
 import io.ktor.application.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import main.java.interfaces.IRepo
+import models.Filter
+
 
 /*
 	Classe responsável pelo manejamento de rotas
@@ -15,10 +18,18 @@ fun Route.MovieRoutes(movieDB: IRepo){
     route("/movies") {
 
         //Rota que retorna todas os movies
-        //TODO:  COMO Q A GNT PEGAR UMA FILTER DO USUARIO
         get{
-            //return@get call.respondText(KtorControllerFabric(movieDB).getAllMovies(filter))
-            return@get call.respondText(KtorControllerFabric(movieDB).getAllMovies())
+            val name = call.request.queryParameters["name"]!!
+            val theme = call.request.queryParameters["theme"]!!
+            val available = call.request.queryParameters["available"]!!
+
+            val filter = Filter(name,theme,available)
+
+            val mapper = jacksonObjectMapper()
+            val filterJSON = mapper.writeValueAsString(filter)
+
+            return@get call.respondText(KtorControllerFabric(movieDB).getAllMovies(filterJSON))
+            //return@get call.respondText(KtorControllerFabric(movieDB).getAllMovies())
         }
 
         //Rota que retorna um review
