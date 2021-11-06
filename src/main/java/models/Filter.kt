5e013d9e8a46714c71e7f8a0bea804f40bb0b4ce
F7Villani <felipevillani.2000@hotmail.com>
2021-11-06 14:21:27 -0300

    Create Filter.kt

---
+++ src/main/java/models/Filter.kt
@@ -0,0 +1,12 @@
+package models
+
+/*
+    Model de Filter
+    Atributos:
+        name: int
+        theme: string
+        available: string
+ */
+data class Filter(
+    val name: String = "", val theme: String = "", val available: String = ""
+)
